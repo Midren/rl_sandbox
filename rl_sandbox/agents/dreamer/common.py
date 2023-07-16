Fix KL div calculation

rl_sandbox/agents/dreamer/common.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import torch.distributions as td
 
 from rl_sandbox.utils.dists import DistLayer
 
@@ -15,7 +16,7 @@ class View(nn.Module):
 
 
 def Dist(val):
-    return DistLayer('onehot')(val)
+    return td.Independent(DistLayer('onehot')(val), 1)
 
 
 class Normalizer(nn.Module):

rl_sandbox/agents/dreamer/world_model.py
@@ -127,8 +127,8 @@ class WorldModel(nn.Module):
     def get_initial_state(self, batch_size: int = 1, seq_size: int = 1):
         device = next(self.parameters()).device
         return State(torch.zeros(seq_size, batch_size, self.rssm_dim, device=device),
-                            torch.zeros(seq_size, batch_size, self.latent_classes, self.latent_dim, device=device),
-                            torch.zeros(seq_size, batch_size, self.latent_classes * self.latent_dim, device=device))
+                     torch.zeros(seq_size, batch_size, self.latent_classes, self.latent_dim, device=device),
+                     torch.zeros(seq_size, batch_size, self.latent_classes * self.latent_dim, device=device))
 
     def predict_next(self, prev_state: State, action):
         prior, _ = self.recurrent_model.predict_next(prev_state, action)

rl_sandbox/agents/dreamer/world_model_slots.py
@@ -236,11 +236,11 @@ class WorldModel(nn.Module):
 
         def KL(dist1, dist2):
             KL_ = torch.distributions.kl_divergence
-            kl_lhs = KL_(td.OneHotCategoricalStraightThrough(logits=dist2.detach()),
-                         td.OneHotCategoricalStraightThrough(logits=dist1)).mean()
+            kl_lhs = KL_(td.Independent(td.OneHotCategoricalStraightThrough(logits=dist2.detach()), 1),
+                         td.Independent(td.OneHotCategoricalStraightThrough(logits=dist1), 1)).mean()
             kl_rhs = KL_(
-                td.OneHotCategoricalStraightThrough(logits=dist2),
-                td.OneHotCategoricalStraightThrough(logits=dist1.detach())).mean()
+                td.Independent(td.OneHotCategoricalStraightThrough(logits=dist2), 1),
+                td.Independent(td.OneHotCategoricalStraightThrough(logits=dist1.detach()), 1)).mean()
             kl_lhs = torch.maximum(kl_lhs, self.kl_free_nats)
             kl_rhs = torch.maximum(kl_rhs, self.kl_free_nats)
             return ((self.alpha * kl_lhs + (1 - self.alpha) * kl_rhs))

rl_sandbox/agents/dreamer/world_model_slots_attention.py
@@ -115,8 +115,8 @@ class WorldModel(nn.Module):
         if decode_vit:
             self.dino_predictor = Decoder(rssm_dim + latent_dim * latent_classes,
                                           norm_layer=nn.GroupNorm if layer_norm else nn.Identity,
-                                          conv_kernel_sizes=[3],
-                                          channel_step=2*self.vit_feat_dim,
+                                          conv_kernel_sizes=[],
+                                          channel_step=self.vit_feat_dim,
                                           kernel_sizes=self.decoder_kernels,
                                           output_channels=self.vit_feat_dim+1,
                                           return_dist=False)
@@ -243,11 +243,11 @@ class WorldModel(nn.Module):
 
         def KL(dist1, dist2):
             KL_ = torch.distributions.kl_divergence
-            kl_lhs = KL_(td.OneHotCategoricalStraightThrough(logits=dist2.detach()),
-                         td.OneHotCategoricalStraightThrough(logits=dist1)).mean()
+            kl_lhs = KL_(td.Independent(td.OneHotCategoricalStraightThrough(logits=dist2.detach()), 1),
+                         td.Independent(td.OneHotCategoricalStraightThrough(logits=dist1), 1)).mean()
             kl_rhs = KL_(
-                td.OneHotCategoricalStraightThrough(logits=dist2),
-                td.OneHotCategoricalStraightThrough(logits=dist1.detach())).mean()
+                td.Independent(td.OneHotCategoricalStraightThrough(logits=dist2), 1),
+                td.Independent(td.OneHotCategoricalStraightThrough(logits=dist1.detach()), 1)).mean()
             kl_lhs = torch.maximum(kl_lhs, self.kl_free_nats)
             kl_rhs = torch.maximum(kl_rhs, self.kl_free_nats)
             return ((self.alpha * kl_lhs + (1 - self.alpha) * kl_rhs))
@@ -262,11 +262,8 @@ class WorldModel(nn.Module):
 
         self.last_attn = torch.zeros((self.slots_num, self.slots_num), device=a_c.device)
 
-        if self.use_prev_slots:
-            prev_slots = self.slot_attention.generate_initial(b // self.cluster_size).repeat(self.cluster_size, 1, 1, 1).transpose(0, 1)
-            slots_c = self.slot_attention(pre_slot_features_c.flatten(0, 1), prev_slots.flatten(0, 1)).reshape(b // self.cluster_size, self.cluster_size, self.slots_num, -1)
-        else:
-            slots_c = self.slot_attention(pre_slot_features_c.flatten(0, 1)).reshape(b, seq_num, self.slots_num, -1)
+        prev_slots = self.slot_attention.generate_initial(b // self.cluster_size).repeat(self.cluster_size, 1, 1, 1).transpose(0, 1)
+        slots_c = self.slot_attention(pre_slot_features_c.flatten(0, 1), prev_slots.flatten(0, 1)).reshape(b // self.cluster_size, self.cluster_size, self.slots_num, -1)
 
         for t in range(self.cluster_size):
             # s_t <- 1xB^xHxWx3

rl_sandbox/agents/dreamer/world_model_slots_combined.py
@@ -110,8 +110,8 @@ class WorldModel(nn.Module):
         if decode_vit:
             self.dino_predictor = Decoder(rssm_dim + latent_dim * latent_classes,
                                           norm_layer=nn.GroupNorm if layer_norm else nn.Identity,
-                                          conv_kernel_sizes=[3],
-                                          channel_step=2*self.vit_feat_dim,
+                                          conv_kernel_sizes=[],
+                                          channel_step=self.vit_feat_dim,
                                           kernel_sizes=self.decoder_kernels,
                                           output_channels=self.vit_feat_dim+1,
                                           return_dist=False)
@@ -190,6 +190,7 @@ class WorldModel(nn.Module):
             discount_factors = self.discount_predictor(prior.combined).sample()
         else:
             discount_factors = torch.ones_like(reward)
+
         return prior, reward, discount_factors
 
     def get_latent(self, obs: torch.Tensor, action, state: t.Optional[tuple[State, torch.Tensor]]) -> t.Tuple[State, torch.Tensor]:
@@ -297,7 +298,6 @@ class WorldModel(nn.Module):
             else:
                 x_r = td.Independent(td.Normal(torch.sum(decoded_imgs, dim=1), 1.0), 3)
                 img_rec = -x_r.log_prob(obs).float().mean()
-
             losses['loss_reconstruction'] = img_rec
         else:
             if self.vit_l2_ratio != 1.0:

rl_sandbox/metrics.py
@@ -63,7 +63,7 @@ class DreamerMetricsEvaluator():
 
         if self.agent.is_discrete:
             self._action_probs += self._action_probs
-        self._latent_probs += self.agent._state.stoch_dist.probs.squeeze().mean(dim=0)
+        self._latent_probs += self.agent._state.stoch_dist.base_dist.probs.squeeze().mean(dim=0)
 
     def on_episode(self, logger):
         latent_hist = (self._latent_probs / self.stored_steps).detach().cpu().numpy()
@@ -152,7 +152,7 @@ class SlottedDreamerMetricsEvaluator(DreamerMetricsEvaluator):
 
         if self.agent.is_discrete:
             self._action_probs += self._action_probs
-        self._latent_probs += self.agent._state[0].stoch_dist.probs.squeeze().mean(dim=0)
+        self._latent_probs += self.agent._state[0].stoch_dist.base_dist.probs.squeeze().mean(dim=0)
 
     def on_episode(self, logger):
         wm = self.agent.world_model